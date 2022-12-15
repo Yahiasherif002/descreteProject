4157bd725363838c30942d5bc27b4918bee669b0
Yahiasherif002 <120673276+Yahiasherif002@users.noreply.github.com>
2022-12-15 20:08:44 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿int num1, num2;
+Console.WriteLine("Get All Perfect In Range of Between two Number");
+Console.WriteLine("Enter the first number:");
+num1 = (int)double.Parse(Console.ReadLine());
+Console.WriteLine("Enter the second number:");
+num2 = (int)double.Parse(Console.ReadLine());
+Console.WriteLine(" the perfect number between " + num1 + " and " + num2);
+
+for (int i = num1; i <= num2; i++)
+{
+    decimal sum = 0;
+    for (int j = 1; j < i; j++)
+    {
+        if (i % j == 0)
+            sum = sum + j;
+    }
+    if (sum == i)
+        Console.WriteLine("\t" + i);
+}
+
+
+
+
+
+
+
